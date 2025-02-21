00a9ecb466e2e27d16f645305c4269f39e8a5ed4
Claudioprueba <pronuer@outlook.com>
2025-02-21 19:09:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// Create web server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var comments = [];
+
+http.createServer(function(request, response) {
+    var urlParsed = url.parse(request.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.text) {
+        response.setHeader('Cache-control', 'no-cache');
+        response.end(urlParsed.query.text);
+    } else if (urlParsed.pathname == '/comments') {
+        if (urlParsed.query.comment) {
+            comments.push(urlParsed.query.comment);
+        }
+        response.setHeader('Cache-control', 'no-cache');
+        response.end(JSON.stringify(comments));
+    } else {
+        response.statusCode = 404;
+        response.end('Page not found');
+    }
+}).listen(1337, '
